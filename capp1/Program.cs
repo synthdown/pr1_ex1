e13ddbcbdfbb840baf72817a175c71e2f4a43be4
student <soloxpwnz@mail.ru>
2020-09-11 18:06:21 +0300

    Добавьте файлы проекта.

---
+++ capp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pr1_upr1_11_09
+{
+  class Program
+  {
+    struct Person
+    {
+      public string firstName;
+      public string lastName;
+      public int age;
+      public Person(string _firstName, string _lastName, int _age)
+      {
+        firstName = _firstName;
+        lastName = _lastName; age = _age;
+      }
+      public override string ToString()
+      {
+        return firstName + " " + lastName + ", age " + age;
+      }
+
+    }
+
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
+    }
+  }
+}
